4076f343f30cc9cef7fccce4bbac97dea3e5b867
“Vlad_Charches” <uladzislau_charches@epam.com>
2016-08-23 15:16:35 +0300

    Task3 edited by U.Charches

---
+++ task3.py
@@ -0,0 +1,76 @@
+import configparser
+import datetime
+import json
+import schedule
+import psutil
+import time
+
+Config = configparser.ConfigParser()
+Config.read("config.ini")
+file_type = Config.get('common', 'output')
+period = Config.get('common', 'interval')
+
+counter = 1
+
+def convertTodict(psutil_info):
+    """Converts psutil format into dictionary"""
+    value = list(psutil_info)
+    key = psutil_info._fields
+    dict0 = dict(zip(key, value))
+    return dict0
+
+def txt_to_file():
+    #writing info to output.txt file
+    timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+    global counter
+    txt = open("output.txt", "a")
+    txt.write("\n###Snapshot {0} : {1} ###\n".format(counter,timestamp))
+    txt.write("===== System-wide CPU utilization per cpu=====\n{0}\n".format(psutil.cpu_percent(interval=1, percpu=True)))
+    txt.write("===== System-wide Memory usage=====\n{0}\n".format(psutil.virtual_memory()))
+    txt.write("===== System-wide Swap memory statistics=====\n{0}\n".format(psutil.swap_memory()))
+    txt.write("===== System-wide Disk I/O statistics=====\n{0}\n".format(psutil.disk_io_counters()))
+    txt.write("===== System-wide Network I/O statistics=====\n{0}\n".format(psutil.net_io_counters(pernic=True)))
+    counter += 1
+    txt.close()
+    print (timestamp)
+
+
+def json_to_file():
+    #writing info to output.json file
+    timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+    global counter
+
+    json_file = open("output.json", "a")
+    json_file.write("\n\n###Snapshot {0} : {1} ###\n".format(counter, timestamp))
+    json_file.write("\n ===== System-wide CPU utilization per cpu=====\n")
+    json.dump(psutil.cpu_percent(interval=1, percpu=True), json_file, indent=4)
+    json_file.write("\n ===== System Memory usage=====\n")
+    json.dump(convertTodict(psutil.virtual_memory()), json_file, indent=4)
+    json_file.write("\n===== System-wide Swap memory statistics=====\n")
+    json.dump(convertTodict(psutil.swap_memory()), json_file, indent=4)
+    json_file.write("\n ===== System-wide Disk I/O statistics=====\n")
+    json.dump(convertTodict(psutil.disk_io_counters()), json_file, indent=4)
+    json_file.write("\n ===== System-wide Network I/O statistics=====\n")
+    json.dump(psutil.net_io_counters(pernic=True), json_file, indent=4)
+    counter += 1
+    json_file.close()
+    print (timestamp)
+
+
+
+def main():
+    if file_type == "txt":
+        print('Output file type = ' + file_type + ', interval = ' + period + ' seconds')
+        txt_to_file()
+    elif file_type == "json":
+        print('Output filetype = ' + file_type + ', interval = ' + period + ' seconds')
+        json_to_file()
+    else:
+        print("Wrong filetype in config file")
+        quit()
+
+schedule.every(int(period)).seconds.do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(0)
